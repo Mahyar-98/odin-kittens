92682cb6d84cf64235cc84c2e8f70b6c5e668737
Mahyar <mahyar.erfanian1998@gmail.com>
2023-12-17 17:27:07 -0500

    flash messages were added after creation, updating, and deleting

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -15,8 +15,10 @@ class KittensController < ApplicationController
         @kitten = Kitten.new(allowed_params)
 
         if @kitten.save
+            flash[:notice] = "Meow! We have a new kitten!"
             redirect_to @kitten
         else
+            flash.now[:alert] = "Kitten creation was failed!"
             render :new, status: :unprocessable_entity
         end
     end
@@ -29,8 +31,10 @@ class KittensController < ApplicationController
         @kitten = Kitten.find(params[:id])
 
         if @kitten.update(allowed_params)
+            flash[:notice] = "Prrr! Kitten was updated successfully!"
             redirect_to @kitten
         else
+            flash.now[:alert] = "Update was unsuccessful!"
             render :edit, status: :unprocessable_entity
         end
     end
@@ -39,6 +43,7 @@ class KittensController < ApplicationController
         @kitten = Kitten.find(params[:id])
         @kitten.destroy
 
+        flash[:notice] = "A kitten was successfully and tragically deleted!"
         redirect_to root_path, status: :see_other
     end
 
